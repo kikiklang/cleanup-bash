732bd0ccb7738b4f10f2c2b275c01746708dbc68
kikiklang <kikiklang@protonmail.com>
2020-08-14 01:31:01 +0200

    update

--- README.md
+++ README.md
@@ -1,3 +1,23 @@
-Un program bash pour MAJ Ubuntu et nettoyer le système.
+# Ubuntu clean-up
 
-il faut config le fichier avec votre nom d'utilisateur UNIX ( command 'WHO') et le nom de votre disque('sudo fdisk -l')
+Un program bash pour MAJ Ubuntu et nettoyer le système. Je l'utilise avec ubuntu 20.04, je ne garantie pas son bon fonctionnement avec d'anciennes versions... à tester...
+
+
+## Utilisation
+
+1. Cloner le projet
+2. Ouvrir le fichier
+3. Renseigner votre nom d'utilisateur unix ( command  >> 'who')
+4. Renseigner le nom de votre disque dur ( command >> lsblk )
+
+`![Moi c'est nvme0n1p5 ¯\_(ツ)_/¯](screenshot.png)`
+
+## Fonctions
+
+* Check l'espace dispo sur le disque avant et après netoyage
+* Nettoie differents logs
+* Liste les snap et supprime les vieilles version inutiles
+* nettoie le cache des vignettes
+* supprime les vieux kernels
+* vide la corbeille
+* update / upgrade et nettoie les paquets
--- cleanup.sh
+++ cleanup.sh
@@ -3,8 +3,8 @@
 # ----------------------------------
 # User info
 # ----------------------------------
-USER='your_username'
-DISK='your_diskanme' 
+USER='vinz'
+DISK='nvme0n1p5' 
 
 # ----------------------------------
 # Variables
@@ -28,10 +28,11 @@ current_task_title "Taille du disque avant nettoyage"
 df /dev/${DISK} -h --output=source,size,used,pcent
 
 # ----------------------------------
-# Clear systemd journal logs
+# Clear various logs
 # ----------------------------------
-current_task_title "Nettoyage des logs du system"
+current_task_title "Nettoyage des logs"
 sudo journalctl --vacuum-time=3d
+sudo rm -f /var/log/*g
 
 # ----------------------------------
 # List of installed snap and remove older versions 
@@ -50,6 +51,33 @@ snap list --all | awk '/disabled/{print $1, $3}' |
 current_task_title "Supprime le cache des vignettes system"
 rm -rf ~/.cache/thumbnails/*
 echo -e "${GREEN}ok${NOCOLOR}"
+
+# ----------------------------------
+# Remove old kernels 
+# https://github.com/kivisade/kernel-purge/blob/master/kernel-purge.sh
+# ----------------------------------
+current_task_title "Supprime les kernels obsolètes"
+current_kernel="$(uname -r | sed 's/\(.*\)-\([^0-9]\+\)/\1/')"
+current_ver=${current_kernel/%-generic}
+
+echo "La version courante est : ${current_kernel}"
+# uname -a
+
+function xpkg_list() {
+    dpkg -l 'linux-*' | sed '/^ii/!d;/linux-libc-dev/d;/'${current_ver}'/d;s/^[^ ]* [^ ]* \([^ ]*\).*/\1/;/[0-9]/!d'
+}
+
+echo "Les noyaux suivants (désormais inutilisés) vont être supprimés"
+xpkg_list
+
+read -p '-- On continue?  (y / n) --' -n 1 -r
+printf "\n"
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+    xpkg_list | xargs sudo apt-get -y purge
+else
+    echo 'Operation a été annulée - aucun changement effectué'
+fi
 
 # ----------------------------------
 # Empty trash bin
